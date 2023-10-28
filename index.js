b187e0acbf530e8a0db137943e4b1d22a46562ae
Dhruv Naik <dhruvnaik21@gmail.com>
2023-10-28 11:10:23 +0530

    Cleaned up the javascript

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
-for(var i =0 ;i<document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll(".drum")[i].addEventListener( "click",function(){
-        var buttonInnerHTML = this.innerHTML;
-        sound(buttonInnerHTML);
-    });
-}
-
-
-
-
-
-document.addEventListener("keypress",function(event){
-sound(event.key);
-});
-
-function sound(key){
-   
-    
-    switch(key){
-                case "w":
-                        var tom1 =new Audio("sounds/tom-1.mp3")
-                        tom1.play();
-                        break;
-                case "a":
-                        var tom2 = new Audio("sounds/tom-2.mp3")
-                        tom2.play();
-                        break;
-                case "s":
-                        var tom3 =new Audio("sounds/tom-3.mp3")
-                        tom3.play();
-                        break;
-                case "d":
-                        var tom4 =new Audio("sounds/tom-4.mp3")
-                        tom4.play();
-                        break;
-                case "j":
-                        var crash =new Audio("sounds/crash.mp3")
-                        crash.play();
-                        break;
-                case "k":
-                        var kick =new Audio("sounds/kick-bass.mp3")
-                        kick.play();
-                        break;
-                case "l":
-                        var snare =new Audio("sounds/snare.mp3")
-                        snare.play();
-                        break;
-                default:
-                    console.log("dru")                                                                                
-
-}
-}
+const drumKit = {
+        sounds: {
+          w: 'tom-1.mp3',
+          a: 'tom-2.mp3',
+          s: 'tom-3.mp3',
+          d: 'tom-4.mp3',
+          j: 'crash.mp3',
+          k: 'kick-bass.mp3',
+          l: 'snare.mp3',
+        },
+      
+        init() {
+          this.setupEventListeners();
+        },
+      
+        setupEventListeners() {
+          document.addEventListener('keydown', (event) => {
+            this.handleKeyPress(event.key);
+          });
+      
+          const drumButtons = document.querySelectorAll('.drum');
+          drumButtons.forEach((button) => {
+            button.addEventListener('click', (event) => {
+              this.handleButtonClick(event.target.innerHTML);
+            });
+          });
+        },
+      
+        handleKeyPress(key) {
+          if (this.sounds[key]) {
+            this.playSound(this.sounds[key]);
+          } else {
+            this.logError('Invalid key: ' + key);
+          }
+        },
+      
+        handleButtonClick(buttonInnerHTML) {
+          if (this.sounds[buttonInnerHTML]) {
+            this.playSound(this.sounds[buttonInnerHTML]);
+          } else {
+            this.logError('Invalid button: ' + buttonInnerHTML);
+          }
+        },
+      
+        playSound(soundFile) {
+          const audio = new Audio(`sounds/${soundFile}`);
+          audio.play();
+        },
+      
+        logError(message) {
+          console.error(message);
+        },
+      };
+      
+      drumKit.init();
+      
